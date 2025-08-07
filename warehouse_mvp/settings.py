a9766fc53ac306d10f51138976a74aa9f06f451c
Abhijeet Shukla <abhijeet.shukla194@gmail.com>
2025-08-07 12:31:37 +0530

    Prepare project for Render deployment

--- warehouse_mvp/settings.py
+++ warehouse_mvp/settings.py
@@ -134,7 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ALLOWED_HOSTS = ['.onrender.com']
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
